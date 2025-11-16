91960d12525efbe7d991cd1fa7e9f3470ca34615
Dev 3zozz <azozzalfiras@gmail.com>
2025-11-17 01:44:27 +0300

    Initial Commit

---
+++ Quran Aura/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quran Aura
+//
+//  Created by 0x on 17/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quran Aura/Quran_AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quran_AuraApp.swift
+//  Quran Aura
+//
+//  Created by 0x on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Quran_AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
